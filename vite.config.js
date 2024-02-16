0dc7c56558701903e3729b7ca95693029da5080f
Alex Kimeu <alexkimeu1999@gmail.com>
2024-02-16 14:01:44 +0300

    Changed some configuratons in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = '/Bank-of-Flatiron/'
+    config.base = '/Bot-Battlr/'
   }
 
   return config
